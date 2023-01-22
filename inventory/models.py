924cdea26821be867ce74f3746ef72da028e6ff1
Jakub <kubodemko@gmail.com>
2023-01-22 18:26:21 +0100

    Second commit: Admin site register, create models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.FloatField(default=0)
+    quantity_type = models.CharField(max_length=200)
+    price = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name, self.quantity, self.quantity_type, self.price
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name, self.price
+
+class RecipeRequirements(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.menu_item, self.ingredient
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=0)
